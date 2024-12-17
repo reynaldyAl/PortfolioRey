8cdcfca1db2f896d1be06a33b82ec6fd893150b2
reynaldyAl <reynaldy.al123@gmail.com>
2024-12-18 00:48:10 +0800

    Reduce particle movement speed in styling.js for smoother animations

--- assets/js/styling.js
+++ assets/js/styling.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         move: {
           enable: true,
-          speed: 6,
+          speed: 3,
           direction: "none",
           random: false,
           straight: false,
